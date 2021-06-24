44aed0afa4caa851bd7192efa0b347dc9d468cfd
Dhruv Dange <dhruvdange@gmail.com>
2021-06-24 19:55:02 +0530

    created cogs, organised files

--- bot.py
+++ bot.py
@@ -9,80 +9,17 @@ intents = discord.Intents(messages = True, guilds = True, reactions = True, memb
 
 client = commands.Bot(command_prefix = ".", intents = intents)
 
-@client.event
-async def on_ready():
-    print("Bot is ready.")
-
-@client.event
-async def on_member_join(member):
-    print(f"{member} has unfortunately joined this server.")
-
-@client.event
-async def on_member_remove(member):
-    print(f"{member} has left the server!")
-
+@client.command()
+async def load(ctx, extension): # ctx -> context; extension -> cog
+    client.load_extension(f"cogs.{extension}")
 
 @client.command()
-async def ping(ctx):
-    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+async def unload(ctx, extension): # ctx -> context; extension -> cog
+    client.unload_extension(f"cogs.{extension}")
 
-@client.command(aliases = ['toss', 'cointoss'])
-async def coin(ctx):
-    responses = ["Heads", "Tails"]
-    await ctx.send(f"Its {random.choice(responses)}.")
+for filename in os.listdir("./cogs"):
+    if filename.endswith('.py'):
+        client.load_extension(f"cogs.{filename[:-3]}") # To remove .py from filename
 
-@client.command(aliases=['8ball'])
-async def _8ball(ctx, *, question):
-    responses = ["It is certain",
-                                "It is decidedly so", 
-                                "Without a doubt", 
-                                "Yes, definitely",
-                                "You may rely on it", 
-                                "As I see it, yes", 
-                                "Most Likely", 
-                                "Outlook Good",
-                                "Yes", 
-                                "Signs point to yes", 
-                                "Reply hazy, try again", 
-                                "Ask again later",
-                                "Better not tell you now", 
-                                "Cannot predict now", 
-                                "Concentrate and ask again",
-                                "Don't count on it", 
-                                "My reply is no", 
-                                "My sources say no", 
-                                "Outlook not so good", 
-                                "Very Doubtful"]
-
-    await ctx.send(f"Question: {question} \nAnswer: {random.choice(responses)}.")
-
-@client.command()
-async def clear(ctx, amount=5):
-    await ctx.channel.purge(limit=amount)
-
-@client.command()
-async def kick(ctx, member : discord.Member, *, reason=None):
-    await member.kick(reason=reason)
-    await ctx.send(f"Thank fucking god {member} was kicked.")
-
-@client.command()
-async def ban(ctx, member : discord.Member, *, reason=None):
-    await member.ban(reason=reason)
-    await ctx.send(f"{member} was banned.\nAmen")
-
-@client.command()
-async def unban(ctx, *, member):
-    # Member not in server, cant mention
-    # get list of banned users
-    banned_users = await ctx.guild.bans()
-    member_name, member_descriminator = member.split('#')
-    
-    for ban_entry in banned_users:
-        user = ban_entry.user
-
-        if (user.name, user.discriminator) == (member_name, member_descriminator):
-            await ctx.guild.unban(user)
-            await ctx.send(f"Unbanned user {user}.")
-            return
 
 client.run(bot_token)
---
+++ cogs/example.py
@@ -0,0 +1,88 @@
+import discord
+from discord.ext import commands
+import random
+
+class Example(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Bot is ready.")
+
+    @commands.Cog.listener()
+    async def on_member_join(self, ctx, member):
+        print(f"{member} has unfortunately joined this server.")
+        await ctx.send(f"{member} has joined the server.")
+        
+    @commands.Cog.listener()
+    async def on_member_remove(self, ctx, member):
+        print(f"{member} has left the server.")
+        await ctx.send(f"{member} has left the server!")
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f'Pong! {round(self.client.latency * 1000)}ms')
+    
+    @commands.command(aliases = ['toss', 'cointoss'])
+    async def coin(self, ctx):
+        responses = ["Heads", "Tails"]
+        await ctx.send(f"Its {random.choice(responses)}.")
+
+    @commands.command(aliases=['8ball'])
+    async def _8ball(self, ctx, *, question):
+        responses = ["It is certain",
+                                    "It is decidedly so", 
+                                    "Without a doubt", 
+                                    "Yes, definitely",
+                                    "You may rely on it", 
+                                    "As I see it, yes", 
+                                    "Most Likely", 
+                                    "Outlook Good",
+                                    "Yes", 
+                                    "Signs point to yes", 
+                                    "Reply hazy, try again", 
+                                    "Ask again later",
+                                    "Better not tell you now", 
+                                    "Cannot predict now", 
+                                    "Concentrate and ask again",
+                                    "Don't count on it", 
+                                    "My reply is no", 
+                                    "My sources say no", 
+                                    "Outlook not so good", 
+                                    "Very Doubtful"]
+
+        await ctx.send(f"Question: {question} \nAnswer: {random.choice(responses)}.")
+
+    @commands.command()
+    async def clear(self, ctx, amount=5):
+        await ctx.channel.purge(limit=amount)
+
+    @commands.command()
+    async def kick(self, ctx, member : discord.Member, *, reason=None):
+        await member.kick(reason=reason)
+        await ctx.send(f"Thank fucking god {member} was kicked.")
+
+    @commands.command()
+    async def ban(self, ctx, member : discord.Member, *, reason=None):
+        await member.ban(reason=reason)
+        await ctx.send(f"{member} was banned.\nAmen")
+
+    @commands.command()
+    async def unban(self, ctx, *, member):
+        # Member not in server, cant mention
+        # get list of banned users
+        banned_users = await ctx.guild.bans()
+        member_name, member_descriminator = member.split('#')
+        
+        for ban_entry in banned_users:
+            user = ban_entry.user
+
+            if (user.name, user.discriminator) == (member_name, member_descriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f"Unbanned user {user}.")
+                return
+
+def setup(client):
+    client.add_cog(Example(client))
