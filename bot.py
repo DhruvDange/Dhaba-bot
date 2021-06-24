14dbb33e4760ee900ad1d8b1757edf53329242ee
Dhruv Dange <dhruvdange@gmail.com>
2021-06-24 15:44:13 +0530

    added functions for member join and leave

--- bot.py
+++ bot.py
@@ -4,10 +4,21 @@ import os
 
 bot_token = os.getenv("DISCORD_BOT_TOKEN")
 
-client = commands.Bot(command_prefix=".")
+intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
+
+client = commands.Bot(command_prefix = ".", intents = intents)
 
 @client.event
 async def on_ready():
     print("Bot is ready.")
+
+@client.event
+async def on_member_join(member):
+    print(f"{member} has unfortunately joined this server.")
+
+@client.event
+async def on_member_remove(member):
+    print(f"{member} has left the server!")
+
 
 client.run(bot_token)
